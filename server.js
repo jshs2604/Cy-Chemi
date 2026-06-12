0c96c65a04b7a6bb978d543fd9e9a34477ca9907
nooooooon <jshs2604@jeju-s.jje.hs.kr>
2026-06-12 11:19:02 +0900

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,17 @@ const crypto = require("crypto");
 
 const app = express();
 const port = Number(process.env.PORT) || 3000;
-const ROOT = path.join(__dirname, "..");
+const ROOT = __dirname;
 const DATA_FILE = path.join(__dirname, "data.json");
 
 app.use(express.json({ limit: "64kb" }));
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+  if (req.method === "OPTIONS") return res.sendStatus(204);
+  next();
+});
 
 function emptyStore() {
   return { nicknames: {}, boards: {}, guestbooks: {}, ilchons: {} };
